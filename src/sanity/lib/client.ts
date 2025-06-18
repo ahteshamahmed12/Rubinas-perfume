b18881e1cca84a20c55a47cc5adcf995568503b6
ahtesham ahmed <160284482+ahteshamahmed12@users.noreply.github.com>
2025-06-18 16:18:32 +0500

    website

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -2,9 +2,9 @@
 import { createClient } from 'next-sanity';
 
 export const client = createClient({
-  projectId: process.env.SANITY_PROJECT_ID!,
-  dataset: process.env.SANITY_DATASET!,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
   apiVersion: '2024-06-01',
   useCdn: false, // false if using token
-  token: process.env.SANITY_API_TOKEN, // Add this!
+  token: process.env.NEXT_PUBLIC_SANITY_TOKEN, // Add this!
 });
