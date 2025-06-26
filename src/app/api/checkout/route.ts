8bb5608c3da3083b1bbaf131e6295140b0e2e134
ahtesham ahmed <160284482+ahteshamahmed12@users.noreply.github.com>
2025-06-26 12:35:18 +0500

    website

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -39,10 +39,13 @@ export async function POST(req: Request) {
     await newOrder.save();
 
     // Save to Google Sheets
-    const auth = new google.auth.GoogleAuth({
-      keyFile: SERVICE_ACCOUNT_PATH,
-      scopes: ['https://www.googleapis.com/auth/spreadsheets'],
-    });
+    const credentials = JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT!);
+
+const auth = new google.auth.GoogleAuth({
+  credentials,
+  scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+});
+
 
     const sheets = google.sheets({ version: 'v4', auth });
     await sheets.spreadsheets.values.append({
