38715295d9c3782953da78462c3a596bb5d75f5e
ahtesham ahmed <160284482+ahteshamahmed12@users.noreply.github.com>
2025-06-29 12:00:40 +0500

    website

--- src/app/sitemap.xml/route.ts
+++ src/app/sitemap.xml/route.ts
@@ -1,24 +1,24 @@
-import type { MetadataRoute } from 'next'
+import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://rubinafragrance.com',
+      url: "https://rubinafragrance.com",
       lastModified: new Date().toISOString(),
-      changeFrequency: 'yearly',
+      changeFrequency: "yearly",
       priority: 1,
     },
     {
-      url: 'https://rubinafragrance.com/about',
+      url: "https://rubinafragrance.com/about",
       lastModified: new Date().toISOString(),
-      changeFrequency: 'yearly',
+      changeFrequency: "yearly",
       priority: 0.8,
     },
     {
-      url: 'https://rubinafragrance.com/blog',
+      url: "https://rubinafragrance.com/blog",
       lastModified: new Date().toISOString(),
-      changeFrequency: 'yearly',
+      changeFrequency: "yearly",
       priority: 0.5,
     },
-  ]
+  ];
 }
